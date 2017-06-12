161894f522401d7c4682bda7ae9ee69d219ccce6
Sean Coffey <seanecoffey@hotmail.com>
2017-06-12 09:57:22 +0800

    update init.js

--- handlers/init.js
+++ handlers/init.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 guilddb = require('../stores/guilddb.json');
+const commands = require('./handlers/commands.js')
 const HELP_MESSAGE = "```\
         Niles Usage\n\
 ---------------------------\n\
@@ -18,26 +19,26 @@ NOTE - THE FULL LIST OF COMMANDS WILL NOT FUNCTION UNTIL GOOGLE CALENDAR ID IS E
 
 exports.run = async function(message) {
   //PREFIX
-  if (mentioned(message, 'prefix'))
+  if (commands.mentioned(message, 'prefix'))
     message.channel.send(`My prefix in this server is \`${guilddb[message.guild.id]["prefix"]}\`.`);
   if(!guilddb[message.guild.id]["prefix"])
     guildb[message.guild.id]["prefix"] = '!';
   const cmd = message.content.toLowerCase().substring(guilddb[message.guild.id]["prefix"].length).split(' ')[0];
   console.log(cmd);
   //ping
-  if (cmd === 'ping' || mentioned(message, 'ping'))
+  if (cmd === 'ping' || commands.mentioned(message, 'ping'))
       message.channel.send(`:ping_pong: Pong! ${client.pings[0]}ms`);
   //help
-  if (cmd === 'help' || mentioned(message, 'help'))
+  if (cmd === 'help' || commands.mentioned(message, 'help'))
     message.author.send(HELP_MESSAGE);
   //Print setup Instructions
-  if (['setup','init'].includes(cmd) || mentioned(message, ['setup','init'])) {
+  if (['setup','init'].includes(cmd) || commands.mentioned(message, ['setup','init'])) {
     message.channel.send('On the settings page for the Google calendar you would like to use, under \'Share with specific people\', invite the service account for Niles:\
 ``niles-291@niles-169605.iam.gserviceaccount.com`` then use the following commands to complete setup:\n\
 ``!id`` <calendarID> to enter your google calendar ID\n``!tz`` <timezone> to enter your timezone, in the form ``GMT+10:00`` (currently must use GMT and maintain full form)');
   }
   //Entering the Google Calendar ID
-  if (cmd === 'id' || mentioned (message, 'id')) {
+  if (cmd === 'id' || commands.mentioned (message, 'id')) {
     calID = message.content.split(' ')[1];
     if (message.content.indexOf('@') == -1) {
       message.channel.send('I don\'t think that\'s a valid calendar ID... try again');
@@ -50,10 +51,7 @@ exports.run = async function(message) {
         if(m.content.toLowerCase() === 'y' || m.content.toLowerCase() === 'yes') {
           guilddb[message.guild.id]["calendarID"] = calID;
           message.channel.send('Okay, I\'m adding your calendar ID as ``' + calID + '``');
-          fs.writeFile('./stores/guilddb.json', JSON.stringify(guilddb, '','\t'), (err) => {
-            if (err)
-              return console.log(Date() + ' write CalendarID error: ' + err);
-          });
+          writeGuilddb(guilddb);
         } else {
           message.channel.send('Okay, I won\'t do that');
         };
@@ -67,14 +65,11 @@ exports.run = async function(message) {
         calID = message.content.split(' ')[1];
         guilddb[message.guild.id]["calendarID"] = calID;
         message.channel.send('Okay, I\'m adding your calendar ID as ``' + calID + '``');
-        fs.writeFile('./stores/guilddb.json', JSON.stringify(guilddb, '','\t'), (err) => {
-          if (err)
-            return console.log(Date() + ' write CalendarID error: ' + err);
-        });
+        writeGuilddb(guilddb);
       }
     }
     //Entering the Timezone
-    if (cmd === 'tz' || mentioned(message, 'tz')) {
+    if (cmd === 'tz' || commands.mentioned(message, 'tz')) {
       if (message.content.split(' ')[1] == undefined) {
         message.channel.send('Please enter timezone in valid format, i.e. ``GMT+06:00``');
         return
@@ -94,20 +89,16 @@ exports.run = async function(message) {
         } else {
           guilddb[message.guild.id]["timezone"] = tz;
           message.channel.send('Okay, I\'m adding your timezone as ``' + tz + '``');
-          fs.writeFile('./stores/guilddb.json', JSON.stringify(guilddb, '','\t'), (err) => {
-            if (err)
-              return console.log(Date() + ' write tz error: ' + err);
-          });
+          writeGuilddb(guilddb);
         }
       }
     }
   }
 
-//helper functions
-//Should import this from Commands.js
-function mentioned(msg, x) {
-    if (!Array.isArray(x)) {
-        x = [x];
+  //helper
+  function writeGuilddb(guilddb) {
+    fs.writeFile('./stores/guilddb.json', JSON.stringify(guilddb, '','\t'), (err) => {
+      if (err)
+        return console.log(Date() + ' write tz error: ' + err);
+      });
     }
-    return msg.isMentioned(client.user.id) && x.some((c) => msg.content.toLowerCase().includes(c));
-}
