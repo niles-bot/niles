7d4c5e67cc903eab787079f4e82bec1a4c4331c5
Michael C <michael@mchang.name>
2021-01-18 00:13:25 -0500

    updated dockerfile and settings

--- settings.js
+++ settings.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const {google} = require("googleapis");
 
 const SA_KEYPATH = secrets.service_acct_keypath;
+const SA_JSON = JSON.parse(fs.readFileSync(SA_KEYPATH, "uft8"));
 const keyFileAuth = new google.auth.GoogleAuth({
   keyFile: SA_KEYPATH,
   scope: ["https://www.googleapis.com/auth/calendar.events"]
@@ -19,7 +20,7 @@ const oAuth2Client = new google.auth.OAuth2(
 
 module.exports = {
   secrets: secrets,
-  sa_id: secrets.serivce_acct_id,
+  sa_id: SA_JSON.client_email || "", 
   oauth2: (fs.existsSync(OAUTH_KEYPATH) ? oAuth2Client : false),
   sa: (fs.existsSync(SA_KEYPATH) ? keyFileAuth : false)
 };
