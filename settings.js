06f04459c36e4dad40b162548c7ccbf7151f2902
Michael C <michael@mchang.name>
2021-01-22 18:47:50 -0500

    added rigorous checks for oauth2/SA

--- settings.js
+++ settings.js
@@ -2,25 +2,38 @@ const secrets = require("./config/secrets.json");
 const fs = require("fs");
 const {google} = require("googleapis");
 
-const SA_KEYPATH = secrets.service_acct_keypath;
-const SA_JSON = JSON.parse(fs.readFileSync(SA_KEYPATH, "utf8"));
-const keyFileAuth = new google.auth.GoogleAuth({
-  keyFile: SA_KEYPATH,
-  scopes: ["https://www.googleapis.com/auth/calendar.events"]
-});
+let keyFileAuth = false;
+let saId = "";
+try {
+  const SA_KEYPATH = secrets.service_acct_keypath;
+  const SA_JSON = JSON.parse(fs.readFileSync(SA_KEYPATH, "utf8"));
+  saId = SA_JSON.client_email;
+  keyFileAuth = new google.auth.GoogleAuth({
+    keyFile: SA_KEYPATH,
+    scopes: ["https://www.googleapis.com/auth/calendar.events"]
+  });
+} catch (err) {
+  console.log(`Error fetching Service Account: ${err}`);
+}
 
-const OAUTH_KEYPATH = secrets.oauth_acct_keypath;
-let oauthJson = fs.readFileSync(OAUTH_KEYPATH, "utf8");
-let oauthKey = JSON.parse(oauthJson).installed;
 
-const {client_secret, client_id, redirect_uris} = oauthKey;
-const oAuth2Client = new google.auth.OAuth2(
-  client_id, client_secret, redirect_uris[0]
-);
+let oAuth2Client = false;
+try {
+  const OAUTH_KEYPATH = secrets.oauth_acct_keypath;
+  let oauthJson = fs.readFileSync(OAUTH_KEYPATH, "utf8");
+  let oauthKey = JSON.parse(oauthJson).installed;
+  
+  const {client_secret, client_id, redirect_uris} = oauthKey;
+  oAuth2Client = new google.auth.OAuth2(
+    client_id, client_secret, redirect_uris[0]
+  );
+} catch (err) {
+  console.log(`Error fetching OAuth2: ${err}`);
+}
 
 module.exports = {
   secrets,
-  saId: SA_JSON.client_email || "", 
-  oauth2: (fs.existsSync(OAUTH_KEYPATH) ? oAuth2Client : false),
-  sa: (fs.existsSync(SA_KEYPATH) ? keyFileAuth : false)
+  saId, 
+  oauth2: oAuth2Client,
+  sa: keyFileAuth
 };
