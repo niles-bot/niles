eb15f936386b8f18c0dc6895d327e6c6c6c03364
Michael C <michael@mchang.name>
2021-04-13 04:04:46 -0400

    validated & corrected doHelpers

--- handlers/displayoptions.js
+++ handlers/displayoptions.js
@@ -1,6 +1,18 @@
 const log = require("debug")("niles:do");
-const strings = require("./strings.js");
-const { send } = require("./comamnds.js");
+const { i18n } = require("./strings.js");
+
+/**
+ * Send message with deletion timeout
+ * @param {Snowflake} channel - channel to send message in
+ * @param {String} content - content of message
+ * @param {Number} [timeout=5000] - time in milliseconds before message is deleted
+ */
+function send(channel, content, timeout=5000) {
+  channel.send(content)
+    .then((message) => {
+      message.delete({ timeout });
+    });
+}
 
 const doHelpArray = {
   pin: {
@@ -35,8 +47,8 @@ const doHelpArray = {
     type: "binaryNumber"
   }, format: {
     name: "format",
-    choices: ["12", "24"],
-    type: "binaryChoice"
+    choices: [12, 24],
+    type: "binaryChoiceInt"
   }, style: {
     name: "style",
     choices: ["code", "embed"],
@@ -62,16 +74,16 @@ const doHelpArray = {
  * @param {String} value - value passed in
  * @param {Guild} guild - Guild object 
  * @param {String} setting - setting name
- * @returns {String} callback string
+ * @returns {String} response to user
  */
 function doBinary(value, guild, setting) {
   log(`doBinary | ${guild.id} | setting: ${setting} | value: ${value}`);
-  const help = strings.i18n.t(`displayoptions.binary.${setting}`);
-  if (value) {
+  const help = i18n.t(`displayoptions.binary.${setting}`);
+  if (value === "1" || value === "0") {
     guild.setSetting(setting, value); // set value
-    return strings.i18n.t(`displayoptions.binary.${(value === "1" ? "confirmOn" : "confirmOff")}`);
+    return i18n.t(`displayoptions.binary.${(value === "1" ? "confirmOn" : "confirmOff")}`, { help, lng: guild.lng });
   } else {
-    return strings.i18n.t("displayoptions.binary.prompt", { lng: guild.lng, help });
+    return i18n.t("displayoptions.badarg.binary", { lng: guild.lng, help });
   }
 }
 
@@ -80,22 +92,14 @@ function doBinary(value, guild, setting) {
  * @param {String} value - value passed in
  * @param {Guild} guild - Guild object 
  * @param {String} setting - setting name
- * @returns {String} callback string
+ * @returns {String} response to user
  */
 function doBinaryEmbed(value, guild, setting) {
   const curStyle = guild.getSetting("style");
-  const help = strings.i18n.t(`displayoptions.binary.${setting}`);
   log(`doBinaryEmbed | ${guild.id} | setting: ${setting} | value: ${value} | style: ${curStyle}`);
   // if set to code, do not allow
-  if (value) {
-    guild.setSetting(setting, value); // set value
-    return strings.i18n.t(
-      (value === "1" ? "displayoptions.binary.confirmOn"
-        : "displayoptions.binary.confirmOff"),
-      { help, lng: guild.lng});
-  } else {
-    return strings.i18n.t("displayoptions.badarg.binary", { lng: guild.lng, help });
-  }
+  if (curStyle === "embed") return doBinary(value, guild, setting);
+  else return i18n.t("displayoptions.embedonly", { lng: guild.lng });
 }
 
 /**
@@ -103,17 +107,17 @@ function doBinaryEmbed(value, guild, setting) {
  * @param {String} value - value passed in
  * @param {Guild} guild - Guild object 
  * @param {Object} settingObj - setting to change
- * @returns {String} setting name
+ * @returns {String} response to user
  */
 function doChoice(value, guild, settingObj) {
   const setting = settingObj.name;
   log(`doChoice | ${guild.id} | setting: ${setting} | value: ${value}`);
   if (settingObj.choices.includes(value)) {
-    const help = strings.i18n.t(`displayoptions.choice.${setting}`);
+    const help = i18n.t(`displayoptions.choice.${setting}`);
     guild.setSetting(setting, value);
-    return strings.i18n.t("displayoptions.choice.confirm", { lng: guild.lng, help, value });
+    return i18n.t("displayoptions.choice.confirm", { lng: guild.lng, help, value });
   } else {
-    return strings.i18n.t(`displayoptions.badarg.${setting}`);
+    return i18n.t(`displayoptions.badarg.${setting}`);
   }
 }
 
@@ -122,20 +126,22 @@ function doChoice(value, guild, settingObj) {
  * @param {Integer} value - value passed in 
  * @param {Guild} guild - Guild object
  * @param {Object} settingObj - settingsObj object
- * @returns 
+ * @returns {String} response to user
  */
 function doInt(value, guild, settingObj) {
-  log(`doInt | ${guild.id} | setting: ${settingObj.name} | value: ${value}`);
+  const name = settingObj.name;
+  log(`doInt | ${guild.id} | setting: ${name} | value: ${value}`);
   if (value) {
-    const help = strings.i18n.t(`displayoptions.choice.${settingObj.help}`);
+    const valueInt = parseInt(value);
+    const help = i18n.t(`displayoptions.choice.${name}`);
     const setValue =
-      isNaN(value) ? settingObj.default
-        : value > settingObj.max ? settingObj.max
-          : value;
-    guild.setSetting(settingObj.name, setValue);
-    return strings.i18n.t("displayoptions.choice.confirm", { lng: guild.lng, help, value });
+      isNaN(valueInt) ? settingObj.default
+        : valueInt > settingObj.max ? settingObj.max
+          : valueInt;
+    guild.setSetting(name, setValue);
+    return i18n.t("displayoptions.choice.confirm", { lng: guild.lng, help, value: setValue });
   } else {
-    return strings.i18n.t(`displayoptions.badarg.${settingObj.name}`);
+    return i18n.t(`displayoptions.badarg.${name}`);
   }
 }
 
@@ -144,24 +150,25 @@ function doInt(value, guild, settingObj) {
  * @param {[String]} args - arguemnts passed in
  * @param {Guild} guild - Guild object that called command
  * @param {Snowflake} channel - Channel that called command
- * @returns 
+ * @returns {String} response to user
  */
 function doHandler(args, guild, channel) {
   const setting = args[0];
   const value = args[1];
-  // find setting
-  if (!( setting in doHelpArray)) return send(channel, strings.i18n.t("displayoptions.help", { lng: guild.lng }));
+  // if not found return help
+  if (!( setting in doHelpArray)) return send(channel, i18n.t("displayoptions.help", { lng: guild.lng }));
   const settingObj = doHelpArray[setting];
   const settingName = settingObj.name;
   if (settingObj.type === "binaryNumber") {
     send(channel, doBinary(value, guild, settingName));
   } else if (settingObj.type === "binaryChoice") {
     send(channel, doChoice(value, guild, settingObj));
+  } else if (settingObj.type === "binaryChoiceInt") {
+    send(channel, doChoice(parseInt(value), guild, settingObj));
   } else if (settingObj.type === "binaryEmbed") {
     send(channel, doBinaryEmbed(value, guild, settingName));
   } else if (settingObj.type ===  "int") {
-    const valueInt = parseInt(value);
-    send(channel, doInt(valueInt, guild, settingObj));
+    send(channel, doInt(value, guild, settingObj));
   }
 }
 
